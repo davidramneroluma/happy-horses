f04cfb2e5f11adc38b4eb76f1a073d4507e647cf
David Ramnerö <david.ramnero@luma.energy>
2023-10-17 14:53:25 +0200

    horse testing

--- test/test.js
+++ test/test.js
@@ -5,26 +5,23 @@ const { main } = require('../index');
 
 describe('Test 1', () => {
   it('Filstråle is the heaviest horse', async () => {
-    const testcase = await main('heaviest');
-    assert.strictEqual(testcase, 'Filstråle');
+    const heaviestHorse = await main('heaviest');
+    assert.strictEqual(heaviestHorse, 'Filstråle');
   });
 });
 
 describe('Test 2', () => {
   it('Lövilevi is the hungriest horse', async () => {
-    const testcase = await main('Hungriest');
-    assert.strictEqual(testcase, 'Lövilevi');
+    const hungriestHorse = await main('Hungriest');
+    assert.strictEqual(hungriestHorse, 'Lövilevi');
   });
 });
 
 describe('Test 3', () => {
   it('The paddocks are well balanced', async () => {
-    const testcase = await main('whoLivesWhere');
-    const paddocks = JSON.stringify(testcase);
-    console.log(paddocks);
-    assert.strictEqual(
-      paddocks,
-      `{"paddock1":["Brölfale","Lövilevi"],"paddock2":["Filstråle","Himmelnos"]}`,
-    );
+    const paddocks = await main('whoLivesWhere');
+    console.log(JSON.stringify(paddocks));
+    assert.strictEqual(paddocks.paddock1.length, 2);
+    assert.strictEqual(paddocks.paddock2.length, 2);
   });
 });
